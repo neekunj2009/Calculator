e143663ab5f38ee6671d508cc455426499bc03f8
neekunj2009 <76521042+neekunj2009@users.noreply.github.com>
2022-10-21 10:51:18 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-print("Hello, World!") #This is a comment.
+
+def main():
+ 
+ print("Select An operator From bellow")
+ print('''
+ + ADD
+ - SUBTRACT
+ * MULTIPY
+ / DIVIDE 
+ ''')
+ num1=int(input("Enter the Value 1:"))
+ num2=int(input("Enter the Value 2:"))
+ opr=input("Enter The Operator")
+
+ if opr== "+":
+    print(num1+num2)
+ elif opr== "-":
+    print(num1-num2)    
+ elif opr== "*":
+    print(num1*num2)
+ elif opr== "/":
+    print(num1/num2)
+ else:
+    print("INVALID OPERATOR")
+
+ Restart= str(input("Do you want to Reuse it "))
+
+ if Restart == "yes":
+    main()
+ elif Restart == "no":
+  exit()
+main()
